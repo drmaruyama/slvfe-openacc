fa7e1cce18adbc74bb7087685e12898252a85d81
Shun Sakuraba <shun.sakuraba@gmail.com>
2024-04-09 14:03:56 +0900

    Indentation fixed to 3 (findent default)

--- engmain.F90
+++ engmain.F90
@@ -2,24 +2,24 @@
 ! ERmod - Eneregy Representation Module
 ! Copyright (C) 2000-2019 Nobuyuki Matubayasi
 ! Copyright (C) 2010-2019 Shun Sakuraba
-! 
+!
 ! This program is free software; you can redistribute it and/or
 ! modify it under the terms of the GNU General Public License
 ! as published by the Free Software Foundation; either version 2
 ! of the License, or (at your option) any later version.
-! 
+!
 ! This program is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-! 
+!
 ! You should have received a copy of the GNU General Public License
 ! along with this program; if not, write to the Free Software
 ! Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
 ! engmain.f90: various parameters about energy calculation
-! 
+!
 ! Angstrom and kcal/mol are taken as the units of length and energy.
 !
 !  names of parameters
@@ -154,7 +154,7 @@
 !   force_calculation: if set to .true.,
 !                      the program continues to run even if there is a warning
 !   stdout : standard output
-!   
+!
 !
 !  names of constants and variables for trajectory generation
 !   moltype : type of the molecule numbering between 1 and numtype
@@ -240,183 +240,183 @@
 !   ecfbin : fine linear mesh for the solute-solvent energy
 !   ec0bin : very fine linear mesh for the solute-solvent energy near 0
 !   finfac : additional "margin" is set in the low-energy domain
-!                       by shifting ecdmin and ecfmns by finfac * ecfbin 
+!                       by shifting ecdmin and ecfmns by finfac * ecfbin
 !
 !
 module engmain
 !
-  implicit none
-  ! Note for optimization: any major compilers shall inline expand "parameter"s
-  ! mathematical & physical constants
-  real, parameter :: PI = 3.1415926535897932
-  real, parameter :: cal_per_joule = 4.1840   ! thermochemical cal / J
+   implicit none
+   ! Note for optimization: any major compilers shall inline expand "parameter"s
+   ! mathematical & physical constants
+   real, parameter :: PI = 3.1415926535897932
+   real, parameter :: cal_per_joule = 4.1840   ! thermochemical cal / J
 !
-  integer :: numtype, nummol, numatm, maxcnf, engdiv, skpcnf, corrcal, selfcal
-  integer :: slttype, wgtslf, wgtsys, wgtins, boxshp, estype
-  integer, parameter :: max_for_spec = 100    ! maximum number of species
-  integer :: sltspec, hostspec(max_for_spec), refspec(max_for_spec)
-  integer :: insorigin, insposition, insorient, insstructure
-  integer :: sltpick, refpick, inscnd, inscfg           ! deprecated
-  real :: lwreg, upreg, lwstr, upstr
-  integer :: ljformat, ljswitch, iseed
-  real :: inptemp, temp
-  integer :: ermax_limit
-  real :: block_threshold
-  logical :: force_calculation
+   integer :: numtype, nummol, numatm, maxcnf, engdiv, skpcnf, corrcal, selfcal
+   integer :: slttype, wgtslf, wgtsys, wgtins, boxshp, estype
+   integer, parameter :: max_for_spec = 100    ! maximum number of species
+   integer :: sltspec, hostspec(max_for_spec), refspec(max_for_spec)
+   integer :: insorigin, insposition, insorient, insstructure
+   integer :: sltpick, refpick, inscnd, inscfg           ! deprecated
+   real :: lwreg, upreg, lwstr, upstr
+   integer :: ljformat, ljswitch, iseed
+   real :: inptemp, temp
+   integer :: ermax_limit
+   real :: block_threshold
+   logical :: force_calculation
 
 
-  ! IO units and files
-  ! system trajectory and setups
-  character(len=*), parameter :: trjfile = 'HISTORY'      ! trajectory filename
-  character(len=*), parameter :: inffile = 'MDinfo'       ! MD info filename
-  integer, parameter :: io_MDinfo = 93                    ! MD info file IO
-  character(len=*), parameter :: ene_confname = 'parameters_er'
-  integer, parameter :: io_paramfile = 91
-  integer, parameter :: io_flcuv = 99      ! IO for flcuv.tt and progress.tt
-  integer, parameter :: stdout = 6         ! standard output
+   ! IO units and files
+   ! system trajectory and setups
+   character(len=*), parameter :: trjfile = 'HISTORY'      ! trajectory filename
+   character(len=*), parameter :: inffile = 'MDinfo'       ! MD info filename
+   integer, parameter :: io_MDinfo = 93                    ! MD info file IO
+   character(len=*), parameter :: ene_confname = 'parameters_er'
+   integer, parameter :: io_paramfile = 91
+   integer, parameter :: io_flcuv = 99      ! IO for flcuv.tt and progress.tt
+   integer, parameter :: stdout = 6         ! standard output
 
-  ! interaction parameters, see setconf.F90
-  character(len=*), parameter :: solute_file = 'SltInfo'  ! solute species
-  character(len=*), parameter :: solvent_file = 'MolPrm'  ! solvent species
-  character(len=*), parameter :: ljtable_file = 'LJTable' ! table for LJ
-  integer, parameter :: mol_io = 79        ! IO for SltInfo and MolPrmX
-  integer, parameter :: ljtable_io = 70    ! IO for LJ table
+   ! interaction parameters, see setconf.F90
+   character(len=*), parameter :: solute_file = 'SltInfo'  ! solute species
+   character(len=*), parameter :: solvent_file = 'MolPrm'  ! solvent species
+   character(len=*), parameter :: ljtable_file = 'LJTable' ! table for LJ
+   integer, parameter :: mol_io = 79        ! IO for SltInfo and MolPrmX
+   integer, parameter :: ljtable_io = 70    ! IO for LJ table
 
-  ! single-solute trajectory
-  !      used only when slttype = SLT_REFS_FLEX, see insertion.F90
-  character(len=*), parameter :: slttrj = 'SltConf'  ! solute trajectory
+   ! single-solute trajectory
+   !      used only when slttype = SLT_REFS_FLEX, see insertion.F90
+   character(len=*), parameter :: slttrj = 'SltConf'  ! solute trajectory
 
-  ! configuration-dependent weight for system used when wgtsys = YES
-  character(len=*), parameter :: syswgt_file = "SysWght"  ! filename
-  integer, parameter :: syswgt_io = 33                    ! file IO
-  ! configuration-dependent weight for single solute used when wgtins = YES
-  !      effective only when slttype = SLT_REFS_FLEX, see insertion.F90
-  character(len=*), parameter :: sltwgt_file = 'SltWght'  ! filename
-  integer, parameter :: sltwgt_io = 31                    ! file IO
+   ! configuration-dependent weight for system used when wgtsys = YES
+   character(len=*), parameter :: syswgt_file = "SysWght"  ! filename
+   integer, parameter :: syswgt_io = 33                    ! file IO
+   ! configuration-dependent weight for single solute used when wgtins = YES
+   !      effective only when slttype = SLT_REFS_FLEX, see insertion.F90
+   character(len=*), parameter :: sltwgt_file = 'SltWght'  ! filename
+   integer, parameter :: sltwgt_io = 31                    ! file IO
 
-  ! reference structure, see insertion.F90
-  !   insorigin = INSORG_REFSTR: solvent species as superposition reference
-  !      refspec : solvent species used as the reference
-  !   insstructure = INSSTR_RMSD: solute itself as superposition reference
-  character(*), parameter :: refstr_file = 'RefInfo'      ! structure filename
-  integer, parameter :: refstr_io = 71                    ! structure file IO
+   ! reference structure, see insertion.F90
+   !   insorigin = INSORG_REFSTR: solvent species as superposition reference
+   !      refspec : solvent species used as the reference
+   !   insstructure = INSSTR_RMSD: solute itself as superposition reference
+   character(*), parameter :: refstr_file = 'RefInfo'      ! structure filename
+   integer, parameter :: refstr_io = 71                    ! structure file IO
 
-  ! used-defined energy coordinate
-  !      effective only when ecprread = YES or meshread = YES, see engproc.F90
-  character(*), parameter :: ecdinfo_file = 'EcdInfo'     ! filename
-  integer, parameter :: ecdinfo_io = 95                   ! file IO
-  character(*), parameter :: ecdmesh_file = 'EcdMesh'     ! filename
-  integer, parameter :: ecdmesh_io = 95                   ! file IO
+   ! used-defined energy coordinate
+   !      effective only when ecprread = YES or meshread = YES, see engproc.F90
+   character(*), parameter :: ecdinfo_file = 'EcdInfo'     ! filename
+   integer, parameter :: ecdinfo_io = 95                   ! file IO
+   character(*), parameter :: ecdmesh_file = 'EcdMesh'     ! filename
+   integer, parameter :: ecdmesh_io = 95                   ! file IO
 
-  ! permutation of atom index, see setconf.F90
-  character(len=*), parameter :: perm_file = "PermIndex"  ! filename
-  integer, parameter :: perm_io = 75                      ! file IO
-
-
-  integer, dimension(:), allocatable :: moltype, numsite, sluvid
-  real, dimension(:,:),  allocatable :: bfcoord
-  real, dimension(:),    allocatable :: sitemass, charge, ljene, ljlen
-
-  integer                            :: ljtype_max
-  integer, dimension(:), allocatable :: ljtype
-  real, dimension(:,:),  allocatable :: ljlensq_mat, ljene_mat
-  
-  real, dimension(:,:),  allocatable :: sitepos
-  real, dimension(:),    allocatable :: mol_charge
-  integer, dimension(:), allocatable :: mol_begin_index, belong_to
-  real, dimension(3,3)               :: cell, invcl
-  real, dimension(3)                 :: celllen
-  real                               :: volume
-
-  real :: elecut, lwljcut, upljcut, screen, ewtoler
-  character(len=8) :: scrtype
-  integer :: intprm, cmbrule, cltype, splodr
-  integer :: ew1max, ew2max, ew3max, ms1max, ms2max, ms3max
-  
-  integer :: ermax, numslv, esmax, maxins
-  integer, dimension(:), allocatable :: uvmax, uvsoft, uvspec
-  real, dimension(:),    allocatable :: uvcrd, edens
-  real, dimension(:,:),  allocatable :: ecorr
-  real, dimension(:),    allocatable :: escrd, eself
-  real, dimension(:,:),  allocatable :: aveuv
-  real, dimension(:),    allocatable :: slnuv
-  real, dimension(:,:),  allocatable :: avediv
-  real                               :: avslf
-  real, dimension(:),    allocatable :: minuv, maxuv
-  integer                            :: numslt
-  integer, dimension(:), allocatable :: sltlist
-  real :: stat_weight_system
-  real :: engnorm, engsmpl, voffset
-  logical :: voffset_initialized = .false.
+   ! permutation of atom index, see setconf.F90
+   character(len=*), parameter :: perm_file = "PermIndex"  ! filename
+   integer, parameter :: perm_io = 75                      ! file IO
 
 
-  ! numeric constants reference
-  integer, parameter :: NO = 0, YES = 1
-  integer, parameter :: SYS_NONPERIODIC = 0, SYS_PERIODIC = 1
-  integer, parameter :: ES_NVT = 1, ES_NPT = 2
-  integer, parameter :: LJFMT_EPS_cal_SGM_nm = 0, LJFMT_EPS_Rminh = 1, &
-                        LJFMT_EPS_J_SGM_A = 2, LJFMT_A_C = 3, &
-                        LJFMT_C12_C6 = 4, LJFMT_TABLE = 5
-  integer, parameter :: LJSWT_POT_CHM = 0, LJSWT_POT_GMX = 1, &
-                        LJSWT_FRC_CHM = 2, LJSWT_FRC_GMX = 3
-  integer, parameter :: LJCMB_ARITH = 0, LJCMB_GEOM = 1
-  integer, parameter :: EL_COULOMB = 0, EL_EWALD = 1, EL_PME = 2, EL_PPPM = 3
-  integer, parameter :: SLT_SOLN = 1, SLT_REFS_RIGID = 2, SLT_REFS_FLEX = 3
-  integer, parameter :: PT_SOLVENT = 0, &
-                        PT_SOLUTE = SLT_SOLN, PT_TEST_RIGID = SLT_REFS_RIGID, &
-                                              PT_TEST_FLEX = SLT_REFS_FLEX
-  ! PT_SOLUTE to PT_TEST_FLEX should correspond to SLT_SOLN to SLT_REFS_FLEX
-  integer, parameter :: INSORG_ORIGIN = 0, INSORG_NOCHANGE= 1, &
-                        INSORG_AGGCEN = 2, INSORG_REFSTR = 3
-  integer, parameter :: INSPOS_RANDOM = 0, INSPOS_NOCHANGE= 1, &
-                        INSPOS_SPHERE = 2, &
-                        INSPOS_SLAB_GENERIC = 3, INSPOS_SLAB_SYMMETRIC = 4, &
-                        INSPOS_RMSD = 5, INSPOS_GAUSS = 6
-  integer, parameter :: INSROT_RANDOM = 0, INSROT_NOCHANGE= 1
-  integer, parameter :: INSSTR_NOREJECT = 0, INSSTR_RMSD = 1
+   integer, dimension(:), allocatable :: moltype, numsite, sluvid
+   real, dimension(:,:),  allocatable :: bfcoord
+   real, dimension(:),    allocatable :: sitemass, charge, ljene, ljlen
 
-  namelist /ene_param/ iseed, &
-       skpcnf, corrcal, selfcal, &
-       slttype, wgtslf, wgtsys, wgtins, boxshp, estype, &
-       sltspec, hostspec, refspec, lwreg, upreg, lwstr, upstr, &
-       insposition, insorient, insstructure, &
-       sltpick, refpick, inscnd, inscfg, &                  ! deprecated
-       ljformat, ljswitch, &
-       inptemp, temp, &
-       engdiv, maxins, &
-       intprm, elecut, lwljcut, upljcut, &
-       cmbrule, cltype, screen, ewtoler, splodr, scrtype, &
-       ew1max, ew2max, ew3max, ms1max, ms2max, ms3max, &
-       ermax_limit, block_threshold, force_calculation
+   integer                            :: ljtype_max
+   integer, dimension(:), allocatable :: ljtype
+   real, dimension(:,:),  allocatable :: ljlensq_mat, ljene_mat
 
-contains 
-  subroutine init_params()
-    implicit none
-    integer :: param_err
-    
-    param_err = 0
-    open(unit = io_paramfile, file = ene_confname, action = "read", iostat = param_err)
-    
-    if(param_err == 0) then
-       read(io_paramfile, nml = ene_param)
-       close(io_paramfile)
-    else
-       stop "parameters_er file does not exist"
-    end if
+   real, dimension(:,:),  allocatable :: sitepos
+   real, dimension(:),    allocatable :: mol_charge
+   integer, dimension(:), allocatable :: mol_begin_index, belong_to
+   real, dimension(3,3)               :: cell, invcl
+   real, dimension(3)                 :: celllen
+   real                               :: volume
 
-  end subroutine init_params
+   real :: elecut, lwljcut, upljcut, screen, ewtoler
+   character(len=8) :: scrtype
+   integer :: intprm, cmbrule, cltype, splodr
+   integer :: ew1max, ew2max, ew3max, ms1max, ms2max, ms3max
 
-  ! returns atom no. for [i]th atom in [mol]th molecule
-  integer function specatm(i, mol)
-    implicit none
-    integer, intent(in) :: i, mol
-    specatm = mol_begin_index(mol) + (i - 1)
-  end function specatm
+   integer :: ermax, numslv, esmax, maxins
+   integer, dimension(:), allocatable :: uvmax, uvsoft, uvspec
+   real, dimension(:),    allocatable :: uvcrd, edens
+   real, dimension(:,:),  allocatable :: ecorr
+   real, dimension(:),    allocatable :: escrd, eself
+   real, dimension(:,:),  allocatable :: aveuv
+   real, dimension(:),    allocatable :: slnuv
+   real, dimension(:,:),  allocatable :: avediv
+   real                               :: avslf
+   real, dimension(:),    allocatable :: minuv, maxuv
+   integer                            :: numslt
+   integer, dimension(:), allocatable :: sltlist
+   real :: stat_weight_system
+   real :: engnorm, engsmpl, voffset
+   logical :: voffset_initialized = .false.
 
-  ! helper function that corresponds mol_begin_index
-  integer function mol_end_index(mol)
-    implicit none
-    integer, intent(in) :: mol
-    mol_end_index = mol_begin_index(mol + 1) - 1
-  end function mol_end_index
+
+   ! numeric constants reference
+   integer, parameter :: NO = 0, YES = 1
+   integer, parameter :: SYS_NONPERIODIC = 0, SYS_PERIODIC = 1
+   integer, parameter :: ES_NVT = 1, ES_NPT = 2
+   integer, parameter :: LJFMT_EPS_cal_SGM_nm = 0, LJFMT_EPS_Rminh = 1, &
+      LJFMT_EPS_J_SGM_A = 2, LJFMT_A_C = 3, &
+      LJFMT_C12_C6 = 4, LJFMT_TABLE = 5
+   integer, parameter :: LJSWT_POT_CHM = 0, LJSWT_POT_GMX = 1, &
+      LJSWT_FRC_CHM = 2, LJSWT_FRC_GMX = 3
+   integer, parameter :: LJCMB_ARITH = 0, LJCMB_GEOM = 1
+   integer, parameter :: EL_COULOMB = 0, EL_EWALD = 1, EL_PME = 2, EL_PPPM = 3
+   integer, parameter :: SLT_SOLN = 1, SLT_REFS_RIGID = 2, SLT_REFS_FLEX = 3
+   integer, parameter :: PT_SOLVENT = 0, &
+      PT_SOLUTE = SLT_SOLN, PT_TEST_RIGID = SLT_REFS_RIGID, &
+      PT_TEST_FLEX = SLT_REFS_FLEX
+   ! PT_SOLUTE to PT_TEST_FLEX should correspond to SLT_SOLN to SLT_REFS_FLEX
+   integer, parameter :: INSORG_ORIGIN = 0, INSORG_NOCHANGE= 1, &
+      INSORG_AGGCEN = 2, INSORG_REFSTR = 3
+   integer, parameter :: INSPOS_RANDOM = 0, INSPOS_NOCHANGE= 1, &
+      INSPOS_SPHERE = 2, &
+      INSPOS_SLAB_GENERIC = 3, INSPOS_SLAB_SYMMETRIC = 4, &
+      INSPOS_RMSD = 5, INSPOS_GAUSS = 6
+   integer, parameter :: INSROT_RANDOM = 0, INSROT_NOCHANGE= 1
+   integer, parameter :: INSSTR_NOREJECT = 0, INSSTR_RMSD = 1
+
+   namelist /ene_param/ iseed, &
+      skpcnf, corrcal, selfcal, &
+      slttype, wgtslf, wgtsys, wgtins, boxshp, estype, &
+      sltspec, hostspec, refspec, lwreg, upreg, lwstr, upstr, &
+      insposition, insorient, insstructure, &
+      sltpick, refpick, inscnd, inscfg, &                  ! deprecated
+      ljformat, ljswitch, &
+      inptemp, temp, &
+      engdiv, maxins, &
+      intprm, elecut, lwljcut, upljcut, &
+      cmbrule, cltype, screen, ewtoler, splodr, scrtype, &
+      ew1max, ew2max, ew3max, ms1max, ms2max, ms3max, &
+      ermax_limit, block_threshold, force_calculation
+
+contains
+   subroutine init_params()
+      implicit none
+      integer :: param_err
+
+      param_err = 0
+      open(unit = io_paramfile, file = ene_confname, action = "read", iostat = param_err)
+
+      if(param_err == 0) then
+         read(io_paramfile, nml = ene_param)
+         close(io_paramfile)
+      else
+         stop "parameters_er file does not exist"
+      end if
+
+   end subroutine init_params
+
+   ! returns atom no. for [i]th atom in [mol]th molecule
+   integer function specatm(i, mol)
+      implicit none
+      integer, intent(in) :: i, mol
+      specatm = mol_begin_index(mol) + (i - 1)
+   end function specatm
+
+   ! helper function that corresponds mol_begin_index
+   integer function mol_end_index(mol)
+      implicit none
+      integer, intent(in) :: mol
+      mol_end_index = mol_begin_index(mol + 1) - 1
+   end function mol_end_index
 end module engmain
